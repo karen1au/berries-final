478a121a1393ae7148ed85f03e196f488effbf53
karen1au <kagemikaka@hotmail.com>
2019-04-30 19:55:21 -0400

    install action cable

--- berries-api/app/channels/application_cable/connection.rb
+++ berries-api/app/channels/application_cable/connection.rb
@@ -4,18 +4,15 @@ module ApplicationCable
   
     def connect
         self.current_user = find_verified_user
+        logger.add_tags current_user.email
     end
   
     private
       
     def find_verified_user
         puts "TOKEEEEN", request.params[:token]
-        if verified_user = User.find_by(token: request.params[:token])
-            verified_user
-        else
+        User.find_by(token: request.params[:token]) ||
             reject_unauthorized_connection
-        end
-      end
     end
   end
 end
--- berries-front/src/index.js
+++ berries-front/src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {BrowserRouter as Router} from "react-router-dom"
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import 'semantic-ui-css/semantic.min.css';
+
+// Action Cable setup
+import actionCable from 'actioncable'
+
+const CableApp = {}
+CableApp.cable = actionCable.createConsumer(`ws://localhost:3000/cable`)
+
+// Pass in CableApp as cableApp prop
+ReactDOM.render(<Router><App cableApp={CableApp} /></Router>, document.getElementById('root'));
