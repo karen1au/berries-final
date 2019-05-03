9e8737a59b5d08c30afe99909112138832985563
Tess <tess.thornley@gmail.com>
2019-05-03 22:20:37 +0000

    index and search functions working with current user location

--- berries-front/src/components/InstrumentExperience.jsx
+++ berries-front/src/components/InstrumentExperience.jsx
@@ -1,6 +1,29 @@
 import React, { Component } from 'react'
 import { Form, Button, Select } from 'semantic-ui-react'
 
+const instrumentOptions = [
+  { key: '1', text: 'electric guitar', value: 'electric guitar' },
+  { key: '2', text: 'drums', value: 'drums' },
+  { key: '3', text: 'percussion', value: 'percussion' },
+  { key: '4', text: 'bass', value: 'bass' },
+  { key: '5', text: 'vocals', value: 'vocals' },
+  { key: '6', text: 'acoustic guitar', value: 'acoustic guitar' },
+  { key: '7', text: 'keyboard', value: 'keyboard' },
+  { key: '8', text: 'violin', value: 'violin' },
+  { key: '9', text: 'trumpet', value: 'trumpet' },
+  { key: '10', text: 'saxophone', value: 'saxophone' },
+  { key: '11', text: 'synthesizer', value: 'synthesizer' }
+]
+
+const experienceOptions = [
+  { key: '1', text: '0-2', value: '0-2' },
+  { key: '2', text: '2-4', value: '2-4' },
+  { key: '3', text: '4-6', value: '4-6' },
+  { key: '4', text: '6-8', value: '6-8' },
+  { key: '5', text: '8-10', value: '8-10' },
+  { key: '6', text: '10+', value: '10+' }
+]
+
 class InstrumentExperience extends Component {
   state = {
     instrument: '',
@@ -14,32 +37,10 @@ class InstrumentExperience extends Component {
 
   onClick = () => {
     this.props.addInstrument(this.state.instrument, this.state.experience)
+    this.setState({instrument: '', experience: ''})
   }
 
   render() { 
-
-    const instrumentOptions = [
-      { key: '1', text: 'electric guitar', value: 'electric guitar' },
-      { key: '2', text: 'drums', value: 'drums' },
-      { key: '3', text: 'percussion', value: 'percussion' },
-      { key: '4', text: 'bass', value: 'bass' },
-      { key: '5', text: 'vocals', value: 'vocals' },
-      { key: '6', text: 'acoustic guitar', value: 'acoustic guitar' },
-      { key: '7', text: 'keyboard', value: 'keyboard' },
-      { key: '8', text: 'violin', value: 'violin' },
-      { key: '9', text: 'trumpet', value: 'trumpet' },
-      { key: '10', text: 'saxophone', value: 'saxophone' },
-      { key: '11', text: 'synthesizer', value: 'synthesizer' }
-    ]
-
-    const experienceOptions = [
-      { key: '1', text: '0-2', value: '0-2' },
-      { key: '2', text: '2-4', value: '2-4' },
-      { key: '3', text: '4-6', value: '4-6' },
-      { key: '4', text: '6-8', value: '6-8' },
-      { key: '5', text: '8-10', value: '8-10' },
-      { key: '6', text: '10+', value: '10+' }
-    ]
     
     return (
     <div>
