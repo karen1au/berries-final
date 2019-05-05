b91510a20e9274cfaca0cc9e4ca5c43bdf4c64ef
Tess <tess.thornley@gmail.com>
2019-05-05 18:03:42 +0000

    description column added to db and profile edit page

--- berries-api/db/schema.rb
+++ berries-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_194915) do
+ActiveRecord::Schema.define(version: 2019_05_05_175556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2019_05_02_194915) do
     t.decimal "lat"
     t.decimal "lng"
     t.string "auth_token"
+    t.string "description"
   end
 
   add_foreign_key "chat_users", "chats"
