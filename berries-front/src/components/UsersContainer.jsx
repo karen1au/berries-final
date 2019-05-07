57d23490777f7c7e5585209072a40a7a6d97be8d
PeterBiskupski <pete.biskupski@gmail.com>
2019-05-07 15:08:03 +0000

    resolving conflicts

--- berries-front/src/components/UsersContainer.jsx
+++ berries-front/src/components/UsersContainer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import UserContainer from './UserContainer'
 // import { ActionCable } from 'react-actioncable-provider';
 
-import { Button, Container, Grid, Header, Image, Segment, Card } from 'semantic-ui-react'
+import { Button, Container, Divider, Grid, Header, Image, Popup, Segment, Card } from 'semantic-ui-react'
 
 class UsersContainer extends Component {
   constructor() {
@@ -49,20 +49,23 @@ class UsersContainer extends Component {
         {this.props.users.map(user => {
           return(
 
-
             <div>
 
-              <Grid columns={2} centered divided padded>
-                <Grid.Row>
-                  <Grid.Column width={2}>
-                    <Image bordered circular size='medium' wrapped src={user.avatar}/>
-                  </Grid.Column>
-                  
-                  <Grid.Column width={4}>
-                    <Header as='h3'>{user.name}</Header>
-                    <p>{user.description}</p>
-                    <UserContainer user={user}/>
-                    <Popup 
+              <Divider style={{ "width": "60%", "margin": "auto" }}/>
+            
+              <Card color="grey" style={{margin: "2vw"}}>
+                <Image src={user.avatar} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>{user.name}</Card.Header>
+                  <Card.Meta>
+                    {user.commitment}
+                  </Card.Meta>
+                  <Card.Description>
+                    {user.description}
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Popup 
                       trigger={
                         <Button inverted color='red' 
                           name={user.id}
@@ -73,58 +76,12 @@ class UsersContainer extends Component {
                       content="Request sent!"
                       on="click"
                       position="right center"
-                    />
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-
-              <Divider style={{ "width": "60%", "margin": "auto" }}/>
-
-            </div>
-=======
-            <Card color="grey" style={{margin: "2vw"}}>
-              <Image src={user.avatar} wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>{user.name}</Card.Header>
-                <Card.Meta>
-                  {user.commitment}
-                </Card.Meta>
-                <Card.Description>
-                  {user.description}
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-              <Button inverted color='red' 
-                  name={user.id}
-                  onMouseOver={this.onChange}
-                  onClick={(e) => this.handleConnectClick(e, this.state)}
-                  >Jam</Button>
-                  <UserContainer user={user} onChange={this.onChange} handleConnectClick={this.handleConnectClick}
-                  data={this.state}/>
-
-              </Card.Content>
-            </Card>
+                  />
+                  <UserContainer user={user} onChange={this.onChange} handleConnectClick={this.handleConnectClick} data={this.state}/>
+                </Card.Content>
+              </Card>
             
-            // <Grid columns={2} centered divided padded>
-            //   <Grid.Row>
-            //     <Grid.Column width={4}>
-            //       <Header as='h2'>{user.name}</Header>
-            //       <Image size='small' wrapped src={user.avatar} circular/>
-            //       <UserContainer user={user}/>
-            //       <Button inverted color='red' 
-            //       name={user.id}
-            //       onMouseOver={this.onChange}
-            //       onClick={(e) => this.handleConnectClick(e, this.state)}
-            //       >Jam</Button>
-            //     </Grid.Column>
-                
-            //     <Grid.Column width={4}>
-            //       <Header as='h3'>{user.location}</Header>
-            //       <p>{user.description}</p>
-            //       {/* <Image size='small' wrapped src={user.avatar}/> */}
-            //     </Grid.Column>
-            //   </Grid.Row>
-            // </Grid>
+            </div>
             
           )
         })}        
